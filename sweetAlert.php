b5c5a47e07deb615cbcaaf80f97e7d0e8d293a83
Tarik Manoar <tarikmanoar@gmail.com>
2019-03-23 22:51:06 +0600

    This is demo

--- sweetAlert.php
+++ sweetAlert.php
@@ -9,21 +9,50 @@
 		<title>Hello, world!</title>
 	</head>
 	<body>
-		<h1>Hello, world!</h1>
-		<p class="btn btn-primary" onclick="wow()">Click Me</p>
-		<script type="text/javascript">
-			function wow(){
-				swal({
-  title: "Good job!",
-  text: "You clicked the button!",
-  icon: "success",
-   buttons: {
-    cancel: true,
-    confirm: true,
-  },
-});
-			}
-		</script>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<!-- Button trigger modal -->
+					<button type="button" class="btn btn-danger btn-lg" data-toggle="modal" data-target="#myModal">
+					Click Me
+					</button>
+					<!-- Modal -->
+					<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+						<div class="modal-dialog">
+							<div class="modal-content">
+								<div class="modal-header">
+									<h4 class="modal-title" id="myModalLabel">Modal titleModal titleModal title</h4>
+								</div>
+								<div class="modal-body">
+									...
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+									<a href="index.php"><button type="button" class="btn btn-primary">Save changes</button></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-12">
+					<p class="btn btn-primary" onclick="wow()">Click Me</p>
+					<script type="text/javascript">
+						function wow(){
+							swal({
+								title: "Good job!",
+								text: "You clicked the button!",
+								icon: "success",
+								buttons: {
+									cancel: true,
+									confirm: true,
+								},
+							});
+						}
+					</script>
+				</div>
+			</div>
+		</div>
+		
 		<!-- Optional JavaScript -->
 		<!-- jQuery first, then Popper.js, then Bootstrap JS -->
 		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
